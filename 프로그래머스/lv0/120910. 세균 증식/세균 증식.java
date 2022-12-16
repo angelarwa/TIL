6d042c0dd29d94ea5e46e838793034d81d681b06
ARWA <59240107+angelarwa@users.noreply.github.com>
2022-12-16 15:26:47 +0900

    [level 0] Title: 세균 증식, Time: 0.02 ms, Memory: 78.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120910. 세균 증식/세균 증식.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int n, int t) {
+        int answer = n;
+        for(int i = 0; t>0; t--){
+            answer *= 2;
+            
+        }
+        return answer;
+    }
+}
