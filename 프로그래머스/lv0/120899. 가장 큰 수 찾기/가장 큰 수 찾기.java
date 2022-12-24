452b94172260c3031107a3f9a05c8239e90f87fb
ARWA <59240107+angelarwa@users.noreply.github.com>
2022-12-24 19:31:29 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.01 ms, Memory: 86 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] array) {
+        int[] answer = new int[2];
+        for(int i=0; i<array.length-1; i++){
+            if(array[i+1]>array[i]){
+                answer[0] = array[i+1];
+                answer[1] = i+1;
+                }
+        }
+        return answer;
+    }
+}
